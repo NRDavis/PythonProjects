7f439f8503d170943c238228745ad5c1ef374c1c
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-03 20:37:56 -0400

    Add files via upload
    tracks green objects exceptionally well.

---
+++ playing_with_opencv/greenTracker.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)			# we use the default web camera 
+
+while 1:
+    _, frame = cap.read()				# depending upon what our while loop returns, we read the camera feed
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)     # we convert the capture's color array from standard BlueGreenRed to HueSaturationValue
+    low_red = np.array([65, 10, 10])
+    upper_red = np.array([95, 255, 255])
+
+    mask = cv2.inRange(hsv, low_red, upper_red)                 # we create a bitmap featuring, separating each of the items into 
+
+    res = cv2.bitwise_and(frame, frame, mask = mask)            # we perform an and operation on each of the pixels, setting them high and low
+
+    cv2.imshow("frame", frame)      # we ouput each of the video streams onto the monitor
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break                               # when we press the esc button, we exit the while loop
+    
+cv2.destroyAllWindows()
