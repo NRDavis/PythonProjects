ff9e278b8f1354cf61f308d3ba5192d4ed8834f0
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-04 12:49:54 -0400

    Add files via upload
    updated comments

--- playing_with_opencv/videoThresh.py
+++ playing_with_opencv/videoThresh.py
@@ -10,7 +10,7 @@ while 1:
     retVal, thresh1 = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY)          # we implement a threshold for 
 
     thresh2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    blur = cv2.GaussianBlur(gray, (7,7), 0)     # we use a gaussian blur 
+    blur = cv2.GaussianBlur(gray, (7,7), 0)     # we use a gaussian blur -- if you change the center argument to a higher or lower square prime, you can change the granulation
     thresh3 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
     #cv2.imshow("frame", frame)
