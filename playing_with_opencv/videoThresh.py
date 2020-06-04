ab2b2849ab7a10c1e8b1de9fd4c80b39610a3d2a
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-03 22:46:55 -0400

    Add files via upload
    This script allows us to take a video feed and put it through a series of filters and thresholds to produce an almost animated effect.

---
+++ playing_with_opencv/videoThresh.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)			# displaying the default web cam
+
+while 1:
+    _, frame = cap.read()		# we read the video feed
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)      # we convert video feed to gray
+    retVal, thresh1 = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY)          # we implement a threshold for 
+
+    thresh2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    blur = cv2.GaussianBlur(gray, (7,7), 0)     # we use a gaussian blur 
+    thresh3 = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    #cv2.imshow("frame", frame)
+    #cv2.imshow("gray", gray) 
+    #cv2.imshow("thresh", thresh1)
+    #cv2.imshow("thresh2", thresh2)
+    cv2.imshow("thresh3", thresh3)
+    
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+	
+cv2.destroyAllWindows()
