2d3283423ab5dad02fea3cb1a3802e31f9ca6935
NR Davis <61124122+NRDavis@users.noreply.github.com>
2020-06-29 16:43:07 -0400

    Add files via upload
    adding an item to open all of the applications I typically use for developing in Python3

---
+++ environmentSetup/openDev.py
@@ -0,0 +1,20 @@
+import os
+
+# opening a browser for consulting documentation
+browser = "google-chrome"
+os.system(f"{browser} &")
+
+# opening a text editor
+notepad = "notepad-plus-plus"
+os.system(f"{notepad} &")
+
+
+
+
+
+
+'''
+https://softwareengineering.stackexchange.com/questions/325417/how-to-use-python-to-start-up-multiple-programs-simultaneously
+
+
+'''
